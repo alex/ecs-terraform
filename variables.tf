4a14e83da56845661136fd31ae6f1a17f4ed306e
Alex Gaynor <alex.gaynor@gmail.com>
2015-12-04 11:39:22 -0500

    fixed indentation

--- variables.tf
+++ variables.tf
@@ -1,37 +1,37 @@
 variable "aws_access_key" {
-  description = "The AWS access key."
+    description = "The AWS access key."
 }
 
 variable "aws_secret_key" {
-  description = "The AWS secret key."
+    description = "The AWS secret key."
 }
 
 variable "region" {
-  description = "The AWS region to create resources in."
-  default = "us-east-1"
+    description = "The AWS region to create resources in."
+    default = "us-east-1"
 }
 
 # TODO: support multiple availability zones, and default to it.
 variable "availability_zone" {
-  description = "The availability zone"
-  default = "us-east-1a"
+    description = "The availability zone"
+    default = "us-east-1a"
 }
 
 variable "ecs_cluster_name" {
-  description = "The name of the Amazon ECS cluster."
-  default = "main"
+    description = "The name of the Amazon ECS cluster."
+    default = "main"
 }
 
 variable "amis" {
-  description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
-  # TODO: support other regions.
-  default = {
-    us-east-1 = "ami-ddc7b6b7"
-  }
+    description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
+    # TODO: support other regions.
+    default = {
+        us-east-1 = "ami-ddc7b6b7"
+    }
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+    default = "t2.micro"
 }
 
 variable "ssh_pubkey_file" {
