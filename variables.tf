38f25ea8bb67e9d4ee5f6c81fbe1f0137ae482a4
Luis Cordova <cordoval@gmail.com>
2015-12-07 16:17:12 -0500

    mods

--- variables.tf
+++ variables.tf
@@ -26,7 +26,8 @@ variable "amis" {
     description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
     # TODO: support other regions.
     default = {
-        us-east-1 = "ami-ddc7b6b7"
+        us-west-1 = "ami-59412f39"
+        #us-east-1 = "ami-ddc7b6b7"
     }
 }
 
